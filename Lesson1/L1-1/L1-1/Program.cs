a948b669ff339e95d220ae3fefb3728368badd5d
shabanov1903 <61244002+shabanov1903@users.noreply.github.com>
2021-07-02 15:38:49 +0300

    lesson-1 (#1)
    merge lesson-1 with main

---
+++ Lesson1/L1-1/L1-1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace L1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckNumber(InputNumber());
+
+            // Реализация тестового запуска функции CheckNumber
+            Console.WriteLine("Передача тестовых аргументов для функции CheckNumber:");
+            // Проверка простого чиста 5
+            CheckNumber(5);
+            // Проверка не простого чиста 35
+            CheckNumber(35);
+            // Проверка простого чиста 73
+            CheckNumber(73);
+        }
+
+        // Реализация функции ввода числа пользователя с мониторингом ошибок ввода
+        private static int InputNumber()
+        {
+            int number;
+            while (true)
+            {
+                Console.WriteLine("Введите целое число больше единицы:");
+                try
+                {
+                    number = Convert.ToInt32(Console.ReadLine());
+                    if (number < 2) throw new FormatException();
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка формата ввода!");
+                }
+            }
+            return number;
+        }
+
+        // Функция проверки чисел
+        private static void CheckNumber(int number)
+        {
+            int d = 0;
+            int i = 2;
+
+            while (i < number)
+            {
+                if (number % i == 0)
+                {
+                    d++;
+                }
+                i++;
+            }
+            if (d == 0 && number != 1 && number != 0)
+            {
+                Console.WriteLine($"Введено простое число {number}");
+            }
+            else
+            {
+                Console.WriteLine($"Введено не простое число {number}");
+            }
+        }
+    }
+}
